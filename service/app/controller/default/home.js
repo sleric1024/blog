97eb7265e1a76b8b562ccbdf89bb078c2779f59d
Eric Lu <sleric1024@gmail.com>
2019-11-30 23:58:26 +0800

    Add article connection from DB

--- service/app/controller/default/home.js
+++ service/app/controller/default/home.js
@@ -4,9 +4,25 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    const result = await this.app.mysql.get('blog_content', {});
+    const result = await this.app.mysql.get('react_blog', {});
     console.log(result);
     this.ctx.body = result;
+  }
+
+  async getArticleList() {
+    const sql = 'SELECT article.id as id,' +
+            'article.title as title,' +
+            'article.introduce as introduce,' +
+            'article.addTime as addTime,' +
+            'article.view_count as view_count ,' +
+            '.type.typeName as typeName ' +
+            'FROM article LEFT JOIN type ON article.type_id = type.Id'
+
+    const results = await this.app.mysql.query(sql);
+
+    this.ctx.body = {
+      data: results,
+    };
   }
 }
 
