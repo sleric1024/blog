cd3c3ff1e5afb412ae6d25de5a24793fd7492122
Eric Lu <sleric1024@gmail.com>
2019-12-02 17:56:31 +0800

    Integrate for article list

--- service/app/controller/default/home.js
+++ service/app/controller/default/home.js
@@ -13,7 +13,8 @@ class HomeController extends Controller {
     const sql = 'SELECT article.id as id,' +
             'article.title as title,' +
             'article.introduce as introduce,' +
-            'article.addTime as addTime,' +
+            'article.article_content as article_content,' +
+            "FROM_UNIXTIME(article.addTime,'%Y-%m-%d %H:%i:%s' ) as addTime," +
             'article.view_count as view_count ,' +
             'type.typeName as typeName ' +
             'FROM article LEFT JOIN type ON article.type_id = type.id';
